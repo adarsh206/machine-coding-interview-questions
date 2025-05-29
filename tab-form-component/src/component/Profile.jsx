b5982056333fd893b69ce38213bfeaba4ed62520
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-05-29 23:38:23 +0530

    profile component completed

--- tab-form-component/src/component/Profile.jsx
+++ tab-form-component/src/component/Profile.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
 
-const Profile = () => {
+const Profile = ({ data, setData}) => {
+
+  const { name, email, age, interests, theme } = data;
+
+  const handleDataChange = (e, item) => {
+    setData((prevState) => ({
+      ...prevState,
+      [item] : e.target.value
+    }))
+  }
+
   return (
-    <div>Profile</div>
+    <div className='profile'>
+      <div className='profile-name'>
+        <label>Name : </label>
+        <input className='input-name' type='text' placeholder='write about yourself' value={name} onChange={(e) => handleDataChange(e, "name")}/>
+      </div>
+      <div className='profile-name'>
+        <label>Age : </label>
+        <input className='input-name' type='number' placeholder='write about yourself' value={age} onChange={(e) => handleDataChange(e, "age")}/>
+      </div>
+      <div className='profile-name'>
+        <label>Email : </label>
+        <input className='input-name' type='email' placeholder='write about yourself' value={email} onChange={(e) => handleDataChange(e, "email")}/>
+      </div>
+      <div className='profile-name'>
+        <label>Interests : </label>
+        <input className='input-name' type='text' placeholder='write about yourself' value={interests} onChange={(e) => handleDataChange(e, "interests")}/>
+      </div>
+      <div className='profile-name'>
+        <label>Theme : </label>
+        <input className='input-name' type='text' placeholder='write about yourself' value={theme} onChange={(e) => handleDataChange(e, "theme")}/>
+      </div>
+    </div>
   )
 }
 
--- tab-form-component/src/component/TabForm.jsx
+++ tab-form-component/src/component/TabForm.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Profile from './Profile'
 import Interests from './Interests'
 import Settings from './Settings'
 
 
 const TabForm = () => {
+
+  const [data, setData] = useState({
+    name: "Adarsh",
+    age : "27",
+    email : "adarsh2061998@gmail.com",
+    interests : ["Coding", "Programming"],
+    theme : "dark"
+  })
+
+  const [activeTab, setActiveTab] = useState(0);
 
   const tabs = [
     {
@@ -19,14 +29,21 @@ const TabForm = () => {
     }
   ]
 
+  const ActiveTabComponent = tabs[activeTab].component
+
   return (
     <div>
-      <div>
+      <div className='heading-container'>
         {
-          tabs.map((t) =>  (
-          <div>{t.name}</div>
+          tabs.map((t, index) =>  (
+          <div key={index} className={`heading ${activeTab === index ? 'selected' : ''}`} onClick={() => setActiveTab(index)}>
+            {t.name}
+          </div>
         ))
         }
+      </div>
+      <div className='tab-body'>
+        <ActiveTabComponent data={data} setData={setData}/>
       </div>
     </div>
   )
