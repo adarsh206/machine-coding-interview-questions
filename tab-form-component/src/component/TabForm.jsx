2ef98ddda726bc7668195c797ab14d4c03784ec5
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-05-29 21:05:52 +0530

    all three components build

--- tab-form-component/src/component/TabForm.jsx
+++ tab-form-component/src/component/TabForm.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import Profile from './Profile'
+import Interests from './Interests'
+import Settings from './Settings'
 
 
 const TabForm = () => {
+
+  const tabs = [
+    {
+      name: "Profile",
+      component: Profile
+    },{
+      name: "Interests",
+      component: Interests
+    },{
+      name: "Settings",
+      component: Settings
+    }
+  ]
+
   return (
-    <div>TabForm</div>
+    <div>
+      <div>
+        {
+          tabs.map((t) =>  (
+          <div>{t.name}</div>
+        ))
+        }
+      </div>
+    </div>
   )
 }
 
