d7fc7632068234a00c1a52f81590db455d1a8e71
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-05-30 23:07:59 +0530

    Settings component completed

--- tab-form-component/src/component/Interests.jsx
+++ tab-form-component/src/component/Interests.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 
-const Interests = () => {
+const Interests = ({data, setData}) => {
+  
+  const { interests } = data;
+
   return (
-    <div>Interests</div>
+    <div>
+      <div>
+        <label>
+          <input type='checkbox' name='coding' checked={interests.includes("Coding")}/>
+          Coding
+        </label>
+      </div>
+      <div>
+        <label>
+          <input type='checkbox' name='programming' checked={interests.includes("Programming")}/>
+          Programming
+        </label>
+      </div>
+      <div>
+        <label>
+          <input type='checkbox' name='javascript' checked={interests.includes("Javascript")}/>
+          Javascript
+        </label>
+      </div>
+    </div>
   )
 }
 
--- tab-form-component/src/component/Settings.jsx
+++ tab-form-component/src/component/Settings.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
 
-const Settings = () => {
+const Settings = ({ data, setData}) => {
+
+  const { theme } = data;
+
+  const handleDataChange = (e) => {
+    setData((prevState) => ({
+      ...prevState,
+      theme: e.target.name
+    }))
+  }
+
   return (
-    <div>Settings</div>
+    <div>
+     <div>
+        <label>
+          <input type='radio' name='dark' checked={theme === "dark"} onChange={handleDataChange}/>
+          Dark
+        </label>
+      </div>
+      <div>
+        <label>
+          <input type='radio' name='light' checked={theme === "light"} onChange={handleDataChange}/>
+          Light
+        </label>
+      </div>
+    </div>
   )
 }
 
