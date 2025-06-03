79904c6dfdd6fb695892f31aef4357d6da6a8df3
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-06-03 22:55:54 +0530

    previous and next button added

--- pagination/src/App.jsx
+++ pagination/src/App.jsx
@@ -9,16 +9,36 @@ const ProductCard = ({image, title}) => {
   </div>
 }
 
-const PAGE_SIZE = 10;
+const PAGE_SIZE = 12;
 
 function App() {
 
   const [products, setProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const start = currentPage * PAGE_SIZE;
+  const end = start + PAGE_SIZE;
+
+  const handlePageChange = (n) => {
+    setCurrentPage(n);
+  }
+
+  const goToNextPage = () => {
+    setCurrentPage((prev) => prev + 1)
+  }
+
+   const goToPrevPage = () => {
+    setCurrentPage((prev) => prev - 1)
+  }
  
   const fetchData = async () => {
-    const data = await fetch("https://dummyjson.com/products?limit=100");
-    const json = await data.json();
-    setProducts(json.products);
+   
+    try {
+       const data = await fetch("https://dummyjson.com/products?limit=500");
+       const json = await data.json();
+       setProducts(json.products);
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   useEffect(() => {
@@ -32,16 +52,22 @@ function App() {
   <div className='App'>
     <h1>Pagination</h1>
      <div className='pagination-container'>
+      <button className='page-number' disabled={currentPage === 0}
+      onClick={() => goToPrevPage()}>◀️</button>
       {
-        [...Array(noOfPages).keys().map(n => <span className='page-number' key={n}>{n}</span>)]
+        [...Array(noOfPages).keys().map(n => <button className={"page-number " + (n === currentPage ? "active" : "")} key={n} 
+          onClick={() => handlePageChange(n)}>
+            {n}
+          </button>)]
       }
+      <button className='page-number' disabled={currentPage === noOfPages - 1}
+      onClick={() => goToNextPage()}>▶️</button>
     </div>
     <div className='products-container'>
       {
-        products.map((p) => <ProductCard key={p.id} image={p.thumbnail} title={p.title}/>)
+        products.slice(start, end).map((p) => <ProductCard key={p.id} image={p.thumbnail} title={p.title}/>)
       }
     </div>
-   
    
   </div>
   )
