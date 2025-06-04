a7123f6b9d12c3778828853c2db45da305cb50b4
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-06-04 21:18:37 +0530

    autocomplete search bar started

---
+++ autocomplete-search-bar/src/App.jsx
@@ -0,0 +1,38 @@
+
+import { useEffect, useState } from 'react';
+import './App.css'
+
+function App() {
+
+  const [input, setInput] = useState("");
+  const [results, setResults] = useState([])
+
+  const fetchData = async () => {
+    const data = await fetch("https://dummyjson.com/recipes/search?q=" + input);
+    const json = await data.json();
+    setResults(json?.recipes);
+  }
+
+  useEffect(() => {
+    fetchData();
+  } ,[input])
+
+  return (
+    <div className='App'>
+     <h1>AutoComplete Search Bar</h1>
+     <div>
+      <input type='text' className='search-input' value={input} onChange={(e) => setInput(e.target.value)}/>
+     </div>
+     <div className='results-container'>
+      {
+        results.map((r) =>(
+          <span className='result' key={r.id}>{r.name}</span>
+         ))
+      }
+     </div>
+     
+    </div>
+  )
+}
+
+export default App
