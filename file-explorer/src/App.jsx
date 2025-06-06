e9dec63b3bf5bb0b048beb605b348c81ed9c0385
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-06-06 22:08:37 +0530

    file explorer started

---
+++ file-explorer/src/App.jsx
@@ -0,0 +1,26 @@
+
+import { useState } from 'react'
+import './App.css'
+import json from "./data.json"
+
+function App() {
+
+  const [data, setData] = useState(json);
+
+  return (
+    <div className='App'>
+   <h1>File Explorer</h1>
+   <div className='container'>
+      {
+        data.map((node) => (
+          <div key={node.id}>
+            {node.name}
+          </div>
+        ))
+      }
+   </div>
+    </div>
+  )
+}
+
+export default App
