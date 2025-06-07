7cf58c086a8cead3a8e43d646782694f67afc9d2
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-06-07 21:39:07 +0530

    list component added

--- file-explorer/src/App.jsx
+++ file-explorer/src/App.jsx
@@ -3,22 +3,29 @@ import { useState } from 'react'
 import './App.css'
 import json from "./data.json"
 
+// Render list of objects
+const List = ({list}) => {
+  return (<div className='container'>
+      {
+        list.map((node) => (
+          <div key={node.id}>
+            <span>{node.name}</span>  
+            {node?.children && <List list={node.children}/>}
+          </div>
+          
+        ))
+      }
+   </div>)
+}
+
 function App() {
 
   const [data, setData] = useState(json);
 
   return (
     <div className='App'>
-   <h1>File Explorer</h1>
-   <div className='container'>
-      {
-        data.map((node) => (
-          <div key={node.id}>
-            {node.name}
-          </div>
-        ))
-      }
-   </div>
+    <h1>File Explorer</h1>
+    <List list={data}/>
     </div>
   )
 }
