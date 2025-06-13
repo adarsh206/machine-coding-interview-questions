579b19bec416b9b3ee7ac0f20846cc49ae11e86b
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-06-13 21:25:26 +0530

    otp input completed

--- otp-input/src/App.jsx
+++ otp-input/src/App.jsx
@@ -1,5 +1,5 @@
 
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css'
 
 const OTP_DIGITS_COUNT = 5;
@@ -9,15 +9,28 @@ function App() {
   const [inputArr, setInputArr] = useState(new Array(OTP_DIGITS_COUNT).fill(""));
 
   const refArr = useRef([]);
+
+  useEffect(() => {
+    refArr.current[0]?.focus();
+  }, [])
+
   const handleOnChange = (value, index) => {
 
     if(isNaN(value)) return;
 
     console.log(value)
+    const newValue = value.trim();
     const newArr = [...inputArr];
-    newArr[index] = value.slice(-1);
+    newArr[index] = newValue.slice(-1);
     setInputArr(newArr);
 
+    newValue && refArr.current[index + 1]?.focus();
+  }
+
+  const handleKeyDown = (e, index) => {
+    if(!e.target.value && e.key === "Backspace"){
+      refArr.current[index - 1]?.focus();
+    }
   }
 
   return (
@@ -28,7 +41,9 @@ function App() {
        return <input key={index} type='text' className='otp-input' 
        value={inputArr[index]} 
        ref={(input) =>(refArr.current[index] = input)}
-       onChange={(e) => handleOnChange(e.target.value, index)}/>
+       onChange={(e) => handleOnChange(e.target.value, index)}
+       onKeyDown={(e) => handleKeyDown(e, index)}
+       />
 })
     }
   
