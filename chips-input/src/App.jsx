d3ce3cbcd029b8fe1ba0181ed91a07a322288ab7
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-06-17 19:23:46 +0530

    chips input completed

--- chips-input/src/App.jsx
+++ chips-input/src/App.jsx
@@ -9,15 +9,22 @@ function App() {
 
   // Function to handle key down events
   const handleKeyDown = (e) => {
-    if(e.key === 'Enter') {
+    if(e.key === 'Enter' && inputText.trim() !== '') {
       // logic to add chips
       setChips((prev) => [...prev, inputText]);
       setInputText('');
     }
   }
 
+  const handleDeleteChip = (index) => {
+    // logic to delete chips
+    const copyChips = [...chips];
+    copyChips.splice(index, 1);
+    setChips(copyChips);
+  }
+
   return (
-  <div className='App' style={{ display: "flex", flexDirection: "column", alignItems: "center", margin: "40px 0"}}>
+  <div className='App' style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", margin: "40px 0"}}>
     <h1>Chips Input</h1>
     <input type='text' placeholder='Type a chip and press tag'
     style={{ padding: "8px", width: "200px"}}
@@ -25,11 +32,13 @@ function App() {
     onChange={(e) => setInputText(e.target.value)}
     onKeyDown={(e) => handleKeyDown(e)}
     />
-    <div>
+    <div style={{ display: "flex", flexWrap: "wrap", width: "300px", marginTop: "10px"}}>
     {
       chips.map((chip, index) => (
-        <div key={index} style={{ background: 'gray', margin: "10px", padding: "10px", color: "white", borderRadius: "10px"}}>
+        <div key={index} style={{ background: 'gray', margin: "10px", padding: "10px", color: "white", 
+        borderRadius: "50px", width: "fit-content", display: "flex", alignItems: "center"}}>
           {chip}
+          <button style={{color: "red", marginLeft: "5px"}} onClick={() => handleDeleteChip(index)}>X</button>
           </div>
       ))
     }
