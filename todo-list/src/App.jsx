a2673e926d05045cf1c71e12956641fe2478ab8d
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-06-18 22:10:09 +0530

    todo list completed

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -8,10 +8,12 @@ function App() {
   const [todoList, setTodosList] = useState([]);
 
   const addTodoItem = () => {
+    if(input.trim() === "") return;
+
     const item = {
-      id: 0,
-      text: input,
-      completed: true
+      id: todoList.length + 1,
+      text: input.trim(),
+      completed: false
     }
     setTodosList((prev) => [...prev, item]);
     setInput('');
@@ -19,20 +21,40 @@ function App() {
 
   const toggleCompleted = (id) => {
 
+    setTodosList(
+      todoList.map(t => {
+        if(t.id === id){
+          return {
+            ...t,
+            completed: !t.completed,
+          }
+        }
+        else{
+          return t;
+        }
+      })
+    )
     
+  }
+
+  const deleteTodo = (id) => {
+
+    setTodosList(
+      todoList.filter((t) => (t.id !== id))
+    )
   }
 
   return (
   <div className='App'>
     <h1>Todo List</h1>
-    <input type='text' placeholder='Enter Todo' value={input} onChange={(e) => setInput(e.target.value)}/>
-    <button onClick={() => addTodoItem()}>Add</button>
+    <input className='input' type='text' placeholder='Enter Todo' value={input} onChange={(e) => setInput(e.target.value)}/>
+    <button className='button' onClick={() => addTodoItem()}>Add</button>
     <ul>
       {
-        todoList.map((todo) => <li key={todo.id}>
+        todoList.map((todo) => <li key={todo.id} className='list'>
           <input type='checkbox' checked={todo.completed} onChange={() => toggleCompleted(todo.id)}/>
           <span className={todo.completed ? "strikeThrough" : ""}>{todo.text}</span>
-          <button>Delete</button>
+          <button className='delete' onClick={() => deleteTodo(todo.id)}>Delete</button>
         </li>)
       }
     </ul>
