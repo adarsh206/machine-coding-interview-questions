837e2006915b27e361093a495d4e1169a8c9abec
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-06-22 18:51:14 +0530

    undoable completed

--- undoable-counter/src/App.jsx
+++ undoable-counter/src/App.jsx
@@ -6,6 +6,8 @@ function App() {
 
   const [value, setValue] = useState(0);
   const [history, setHistory] = useState([]);
+  const [redoList, setRedoList] = useState([]);
+  const [undoCount, setUndoCount] = useState(0);
 
   const maintainHistory = (key, prev, curr) => {
     console.log(key, prev, curr)
@@ -28,14 +30,42 @@ function App() {
 
   const handleUndo = () => {
     // Stack LIFO
+    if(history.length){
+      if(undoCount + 1 > 5){
+        alert("you can't undo beyond limit = 5");
+        return;
+      }
+
+      const copyHist = [...history];
+      const firstItem = copyHist.shift();
+      setHistory(copyHist);
+
+      setValue(firstItem.prev);
+      const copyRedoList = [...redoList];
+      copyRedoList.push(firstItem);
+      setRedoList(copyRedoList);
+    }
   }
+
+  const handleRedo = () => {
+    if(redoList.length){
+      const copyRedoList = [...redoList];
+      const poppedValue = copyRedoList.pop();
+      setRedoList(copyRedoList);
+      
+      const { action, prev, curr } = poppedValue;
+      setValue(curr);
+      maintainHistory(action, prev, curr);
+    }
+  }
+
 
   return (
   <div className='App'>
     <h1>Undoable Counter</h1>
     <div className='action-btn'>
       <button onClick={handleUndo}>Undo</button>
-      <button>Redo</button>
+      <button onClick={handleRedo}>Redo</button>
     </div>
 
     <div className='user-action'>
