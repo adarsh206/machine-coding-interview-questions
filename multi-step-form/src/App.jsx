35626493b8d4015eefb87f833c36ea5e840685ef
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-06-22 19:01:37 +0530

    multi step from started

---
+++ multi-step-form/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+   <div className='App'>
+    <form className='container'>
+      <label>Name</label>
+      <input type='text'/>
+      <button>Next</button>
+      
+    </form>
+   </div>
+  )
+}
+
+export default App
