15cdf1a8532d827e4c2340adf1df12b1b0aa4f0c
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-06-22 21:24:43 +0530

    form added

--- multi-step-form/src/App.jsx
+++ multi-step-form/src/App.jsx
@@ -1,15 +1,56 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
 
+  const data = [
+    {
+      id: "name",
+      label: "Name",
+      inputType: "text",
+      buttonName: "Next",
+      placeholder: "Your Name..."
+    },
+      {
+      id: "email",
+      label: "Email",
+      inputType: "email",
+      buttonName: "Next",
+      placeholder: "Your Email..."
+    },
+    {
+      id: "dob",
+      label: "DOB",
+      inputType: "date",
+      buttonName: "Next",
+      placeholder: ""
+    },
+    {
+      id: "password",
+      label: "Password",
+      inputType: "password",
+      buttonName: "Submit",
+      placeholder: ""
+    }
+  ]
+
+  const [forms, setForms] = useState(data);
+  const [index, setIndex] = useState(0);
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIndex((idx) => idx + 1);
+  }
+
+
   return (
    <div className='App'>
-    <form className='container'>
-      <label>Name</label>
-      <input type='text'/>
-      <button>Next</button>
-      
+    <form className='container' onSubmit={handleSubmit}>
+      <label>{forms[index].label}</label>
+      <input type={forms[index].inputType}  placeholder={forms[index].placeholder} />
+      <button>{forms[index].buttonName}</button>     
     </form>
    </div>
   )
