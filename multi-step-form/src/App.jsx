78ead71729d0e441a1677ef3cb7bdbf88175e1da
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-06-23 21:57:01 +0530

    multi step form completed

--- multi-step-form/src/App.jsx
+++ multi-step-form/src/App.jsx
@@ -44,11 +44,13 @@ function App() {
     password: ""
   })
 
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if(index === forms.length - 1){
       console.log("Form Submitted")
+      setIsFormSubmitted(true);
     }
     else{
       setIndex((idx) => idx + 1);
@@ -63,21 +65,37 @@ function App() {
   }
 
   const handleInputChange = (e) => {
-    
+    const id = e.target.id;
+    const val = e.target.value;
+    const copyFormData = {...formData};
+    copyFormData[id] = val;
+    setFormData(copyFormData);
   }
-
+console.log(formData)
 
   return (
    <div className='App'>
-    <form className='container' onSubmit={handleSubmit}>
+   {!isFormSubmitted ? <form className='container' onSubmit={handleSubmit}>
       { index > 0 && <a href='/' onClick={handleBack}>
         Back
       </a>}
       <label>{forms[index].label}</label>
       <input onChange={handleInputChange}
-      type={forms[index].inputType}  placeholder={forms[index].placeholder} />
+      type={forms[index].inputType} id={forms[index].id} value={formData[forms[index].id]}  placeholder={forms[index].placeholder} />
       <button>{forms[index].buttonName}</button>     
     </form>
+    :
+    <div className='main-container'>
+      <h1>Success !</h1>
+      <hr />
+    <div className='container'>
+      <span>Name: {formData.name}</span>
+      <span>Email: {formData.email}</span>
+      <span>DOB: {formData.dob}</span>
+       <span>Password: {formData.password}</span>
+    </div>
+    </div>
+  }
    </div>
   )
 }
