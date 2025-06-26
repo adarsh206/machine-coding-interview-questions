1938738a32d91b329187877b93c25c243f60de27
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-06-26 20:57:55 +0530

    Star Rating Started

---
+++ star-rating/src/App.jsx
@@ -0,0 +1,27 @@
+
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  return (
+  <div className='App'>
+    <h1>Star Rating</h1>
+    <div>
+      {
+        [1, 2, 3, 4, 5].map((num) => (
+          <button onClick={() => setRating(num)}>
+            <span className={`star`}>
+            &#9733;</span>
+            </button>
+        ))
+      }
+    </div>
+  </div>
+  )
+}
+
+export default App
