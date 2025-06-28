58718fb33f5f3df094a04a9f48fdc89963ff14da
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-06-28 17:15:47 +0530

    Random Numbers Array Added

--- memory-game/src/App.jsx
+++ memory-game/src/App.jsx
@@ -2,14 +2,52 @@
 import { useState } from 'react'
 import './App.css'
 
+
+//[1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8]
+const getNums = () => {
+  const list = [];
+  for(let i = 1; i <= 8; i++){
+    list.push(i);
+    list.push(i);
+  }
+  return list;
+}
+
 function App() {
 
+  const [nums, setNums] = useState(getNums());
   const [stage, setStage] = useState('init')
 
+  const randomNums = () => {
+    const copyNums = [...nums];
+    return copyNums.sort(() => Math.random() - 0.5);
+  }
+
+  const handleStart = () => {
+    setStage('start');
+    setNums(randomNums())
+  }
+
+  console.log('nums', nums)
   return (
    <div className='App'>
     <h1>Memory Game</h1>
-    {stage === 'init' && <button>Play Game</button>}
+    {stage === 'init' && <button onClick={handleStart}>Play Game</button>}
+    {
+      stage === "start" && 
+      <div className='game'>
+        <div className='cards'>
+          Hello
+        </div>
+      </div>
+    }
+    {
+      stage === "win" && 
+      <div>
+        <h1>You won the Game !</h1>
+        <button>Play Again</button>
+      </div>
+    }
    </div>
   )
 }
