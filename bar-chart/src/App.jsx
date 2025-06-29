c703810756c516d516fdc8e06e26ef2110fe3d4a
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-06-29 22:01:14 +0530

    api call added

--- bar-chart/src/App.jsx
+++ bar-chart/src/App.jsx
@@ -1,16 +1,38 @@
 
 import { useEffect } from 'react'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+
+  const [freq, setFreq] = useState(undefined);
 
   const fetchNumbers = async () => {
     const url = 'https://www.random.org/integers/?num=200&min=1&max=10&col=1&base=10&format=plain&rnd=new'
     const res = await fetch(url);
-    const data = await res.text();
-    console.log(data)
+    let data = await res.text();
+    data = data.split('\n').filter(Boolean);
+    const map = {};
+    data?.forEach((item) => {
+      if(map[item]){
+        map[item] = map[item] + 1;
+      }
+      else{
+        map[item] = 1
+      }
+    });
+    setFreq(map)
+   
   }
+  console.log(freq);
 
+  // preparing y-axis data
+  // [40, 30, 20, 10, 0]
+  useEffect(() => {
+    if(freq){
+
+    }
+  } ,[freq])
   useEffect(() => {
     fetchNumbers()
   }, [])
