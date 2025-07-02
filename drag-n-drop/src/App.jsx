277f5fd1b9d6a924c5a698e73d578148807e0d37
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-07-02 22:00:14 +0530

    drag n drop started

---
+++ drag-n-drop/src/App.jsx
@@ -0,0 +1,61 @@
+
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const TODO = 'TODO';
+  const DOING = 'DOING';
+  const DONE = 'DONE';
+
+  const [value, setValue] = useState('');
+  const [tasks, setTasks] = useState([])
+
+  const handleChange = (e) => {
+    setValue(e.target.value)
+  }
+
+  const handleKeyDown = (e) => {
+    console.log(e.keyCode);
+    if(e.keyCode === 13){
+      // Enter Pressed
+      const obj = {
+        title: value,
+        status: TODO,
+        id: Date.now()
+      }
+      setTasks((prev) => [...prev, obj]);
+      setValue('')
+    }
+  }
+
+console.log(tasks)
+  return (
+ <div className='App'>
+  <h1>Task Manager</h1>
+  <input type='text' onChange={handleChange} value={value} onKeyDown={handleKeyDown}/>
+  <div className='board'>
+    <div className="todo">
+      <h2 className='todo-col'>Todo</h2>
+      <div className="task-item">
+        Hello
+        <div className="btns">
+          <span className='btn'>✏️</span>
+          <span className='btn'>❌</span>
+        </div>
+      </div>
+    </div>
+
+    <div className="doing">
+      <h2 className='doing-col'>Doing</h2>
+    </div>
+
+    <div className="done">
+      <h2 className='done-col'>Done</h2>
+    </div>
+  </div>
+ </div>
+  )
+}
+
+export default App
