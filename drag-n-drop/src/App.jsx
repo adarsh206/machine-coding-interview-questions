6445e030b228a5d548953179fa3bf6dacb1598e5
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-07-03 21:54:49 +0530

    todo added

--- drag-n-drop/src/App.jsx
+++ drag-n-drop/src/App.jsx
@@ -9,7 +9,8 @@ function App() {
   const DONE = 'DONE';
 
   const [value, setValue] = useState('');
-  const [tasks, setTasks] = useState([])
+  const [tasks, setTasks] = useState([]);
+  const [dragTask, setDragTask] = useState(null);
 
   const handleChange = (e) => {
     setValue(e.target.value)
@@ -29,7 +30,12 @@ function App() {
     }
   }
 
-console.log(tasks)
+  const handleDrag = (e, task) => {
+    setDragTask(task)
+  }
+
+ console.log(dragTask)
+ 
   return (
  <div className='App'>
   <h1>Task Manager</h1>
@@ -37,21 +43,48 @@ console.log(tasks)
   <div className='board'>
     <div className="todo">
       <h2 className='todo-col'>Todo</h2>
-      <div className="task-item">
-        Hello
+      {
+        tasks.length > 0 && tasks.map((task) => (
+        task.status === TODO && <div className="task-item" draggable key={task.id} onDrag={(e) => handleDrag(e, task)}>
+        {task.title}
         <div className="btns">
           <span className='btn'>✏️</span>
           <span className='btn'>❌</span>
         </div>
       </div>
+        ))
+      }
+      
     </div>
 
     <div className="doing">
       <h2 className='doing-col'>Doing</h2>
+      {
+        tasks.length > 0 && tasks.map((task) => (
+        task.status === DOING && <div className="task-item" draggable  key={task.id}>
+        {task.title}
+        <div className="btns">
+          <span className='btn'>✏️</span>
+          <span className='btn'>❌</span>
+        </div>
+      </div>
+        ))
+      }
     </div>
 
     <div className="done">
       <h2 className='done-col'>Done</h2>
+      {
+        tasks.length > 0 && tasks.map((task) => (
+        task.status === DONE && <div className="task-item" draggable key={task.id}>
+        {task.title}
+        <div className="btns">
+          <span className='btn'>✏️</span>
+          <span className='btn'>❌</span>
+        </div>
+      </div>
+        ))
+      }
     </div>
   </div>
  </div>
