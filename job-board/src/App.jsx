0aad448536c1eecf3e3244dcad1b2313aea620c6
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-07-06 18:03:40 +0530

    data added and rendered

--- job-board/src/App.jsx
+++ job-board/src/App.jsx
@@ -6,6 +6,7 @@ import { useState } from 'react';
 function App() {
  
   const [postIDs, setPostIDs] = useState([]);
+  const [postMetadata, setPostMetadata] = useState([]);
 
   const getData = async (url) => {
     try {
@@ -18,6 +19,33 @@ function App() {
     }
   }
 
+ const getFormattedDate = (unixTimeStamp) => {
+    const date = new Date(unixTimeStamp * 1000);
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    const year = date.getFullYear();
+    const formatedDate = `${month}/${day}/${year}`;
+    return formatedDate;
+ }
+
+  const getJobTitle = (text) => {
+    const arr = text.split(/\((YC[^)]+)\)/);
+    if(arr.length > 1){
+      const part1 = arr[0];
+      const part2 = arr[1];
+      return `${part1} ${part2}`;
+    }
+    return "N/A";
+  }
+
+   const getJobInfo = (text) => {
+      const arr = text.split(/\((YC[^)]+)\)/);
+      if(arr.length > 2){
+        return arr[2];
+      }
+      return "N/A";
+  }
+
   const fetchPostMetaData = async (ids) => {
     const apiCalls = ids.map((id) => {
       const url = `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`;
@@ -25,7 +53,23 @@ function App() {
     });
     const results = await Promise.all(apiCalls);
     console.log(results);
+    if(results.length){
+      const newArr = results.map((item) => {
+      const obj = {
+        jobTitle: getJobTitle(item.title),
+        jobInfo: getJobInfo(item.title),
+        date: getFormattedDate(item.time),
+        url: item.url ? item.url : `https://news.ycombinator.com/item?id=${item.id}`
+      }
+      return obj;
+    })
+
+    let copyPostMetadata = [...postMetadata];
+    copyPostMetadata = [...copyPostMetadata, ...newArr];
+    setPostMetadata(copyPostMetadata)
   }
+  }
+    console.log(postMetadata)
 
   const fetchPostIDs = async () => {
     const url = 'https://hacker-news.firebaseio.com/v0/jobstories.json';
@@ -44,6 +88,25 @@ function App() {
   return (
   <div className='App'>
     <h1>Job Board</h1>
+    <div className="cards">
+      {
+        postMetadata?.length === 0 ?
+        <div>Loading...</div>:
+        postMetadata.map((post) => (
+          <a className='card'>
+            <div className='company-info'>
+              {post.jobTitle}
+            </div>
+            <div className="hiring-info">
+              {post.jobInfo}
+            </div>
+            <div className="date">
+              {post.date}
+            </div>
+          </a>
+        ))
+      }
+    </div>
   </div>
   )
 }
