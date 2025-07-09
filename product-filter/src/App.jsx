3926000c0ff27c2a1507f2412f724e2c0c8a7ca9
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-07-09 21:38:50 +0530

    products data added

--- product-filter/src/App.jsx
+++ product-filter/src/App.jsx
@@ -1,18 +1,32 @@
 
+import { useState } from 'react';
 import './App.css'
+import { items } from './item.js';
 
 function App() {
   
   const filters = ['Bags', 'Watches', 'Sports', 'Sunglasses'];
+  const [filteredData, setFilteredData] = useState(items);
 
   return (
    <div className="App">
+    <h1>Products Filter</h1>
     <div className="filters">
       {
         filters.map((item, idx) => (
           <button key={idx}>
             {item}
           </button>
+        ))
+      }
+    </div>
+    <div className="product-list">
+      {
+        filteredData.map((item, idx) => (
+          <div className="item" key={idx}>
+            <p>{item.name}</p>
+            <p className='category'>{item.category}</p>
+          </div>
         ))
       }
     </div>
