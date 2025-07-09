21fb384f79e363794557fc739cb836a81994c331
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-07-09 23:08:57 +0530

    filter function added

--- product-filter/src/App.jsx
+++ product-filter/src/App.jsx
@@ -7,14 +7,28 @@ function App() {
   
   const filters = ['Bags', 'Watches', 'Sports', 'Sunglasses'];
   const [filteredData, setFilteredData] = useState(items);
+  const [activeFilters, setActiveFilters] = useState([]);
 
+  const handleFilterClick = (e) => {
+    const category = e.target.id;
+    console.log(category);
+    if(activeFilters.includes(category)){ // toggle false
+      const filters =  activeFilters.filter((el) => el !== category );
+      setActiveFilters(filters)
+    }
+    else{ // toggle true
+      setActiveFilters([...activeFilters, category])
+    }
+  }
+  console.log('activeFilters', activeFilters)
+  
   return (
    <div className="App">
     <h1>Products Filter</h1>
-    <div className="filters">
+    <div className="filters" onClick={handleFilterClick}>
       {
         filters.map((item, idx) => (
-          <button key={idx}>
+          <button key={idx} id={item}>
             {item}
           </button>
         ))
