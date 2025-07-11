b9d0e56ea2f54482be5c352e547ec7cfff3f1acf
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-07-11 22:19:04 +0530

    data and box added

--- transfer-list/src/App.jsx
+++ transfer-list/src/App.jsx
@@ -1,12 +1,45 @@
 
+import { useState } from 'react'
 import './App.css'
+import { data } from "./data";
 
 function App() {
   
+  const [leftItems, setLeftItems] = useState(data);
+  const [rightItems, setRightItems] = useState([]);
 
   return (
   <div className='App'>
     <h1>Transfer List</h1>
+    <div className="container">
+      <div className="box">
+        {/** left section */}
+        {
+          leftItems.map(({ title, id, checked }) => (
+            <div className="item" id={id} key={id}>
+              {title}
+            </div>
+          ))
+        }
+      </div>
+
+      <div className="actions">
+        {/** buttons */}
+        <button>Left</button>
+        <button>Right</button>
+      </div>
+
+      <div className="box">
+        {/** right section */}
+        {
+          leftItems.map(({ title, id, checked }) => (
+            <div className="item" id={id} key={id}>
+              {title}
+            </div>
+          ))
+        }
+      </div>
+    </div>
   </div>
   )
 }
