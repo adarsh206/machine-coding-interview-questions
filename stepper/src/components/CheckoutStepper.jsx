0c75d77fc6c1bf006a464811df5c109b0c268967
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-08-09 17:26:40 +0530

    functions added

--- stepper/src/components/CheckoutStepper.jsx
+++ stepper/src/components/CheckoutStepper.jsx
@@ -10,23 +10,43 @@ const CheckoutStepper = ({stepsConfig = []}) => {
     }
 
     const handleNext = () => {
-
+        setCurrentStep((prevStep) => {
+            if(prevStep === stepsConfig.length){
+                setIsComplete(true);
+                return prevStep;
+            }
+            else{
+                return prevStep + 1;
+            }
+        })
     }
-    
+
+    const ActiveComponent = stepsConfig[currentStep - 1]?.Component;
+
   return (
     <>
         <div className='stepper'>
         {
             stepsConfig.map((step, index) => {
                 return (
-                    <div key={step.name} className='step'>
-                        <div className='step-number'>{index + 1}</div>
+                    <div key={step.name} className={`step ${currentStep > index + 1 || isComplete ? "complete" : ""} 
+                    ${currentStep === index + 1 ? "active" : ""}`}>
+                        <div className='step-number'>
+                            {
+                                currentStep > index + 1 || isComplete ? (
+                                    <span>&#10003;</span>
+                                ) : (
+                                    index + 1
+                                )
+                            }</div>
                         <div className='step-name'>{step.name}</div>
                     </div>
                 )
             })
         }
         </div>
+
+        <ActiveComponent />
         {
             !isComplete && (
                 <button className='btn' onClick={handleNext}>
