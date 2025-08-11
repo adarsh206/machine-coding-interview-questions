922612e36933cac9ec891fe13ab589034c107506
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-08-11 09:47:20 +0530

    functions added

--- countdown-timer/src/App.jsx
+++ countdown-timer/src/App.jsx
@@ -1,14 +1,48 @@
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
 
   const [isStart, setIsStart] = useState(false);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [timerId, setTimerId] = useState(0);
 
   const handleStart = () => {
     setIsStart(true);
   }
+
+  const handleReset = () => {
+    setIsStart(false);
+  }
+
+  const handleInput = (e) => {
+    
+    const value = parseInt(e.target.value);
+    const id = e.target.id;
+
+    if(id === 'hours'){
+      setHours(value);
+    }
+    else if(id === 'minutes'){
+      setMinutes(value);
+    }
+    else{
+      setSeconds(value);
+    }
+  }
+
+  useEffect(() => {
+    let tid;
+    if(isStart){
+      tid = setInterval(() => {
+
+      }, 1000)
+      setTimerId(tid)
+    }
+  }, [isStart, hours, minutes, seconds])
 
   return (
     <div className="App">
@@ -17,9 +51,9 @@ function App() {
         !isStart && (
           <div className="input-container">
         <div className="input-box">
-          <input id='hours' placeholder='HH' />
-          <input id='minutes' placeholder='MM' />
-          <input id='seconds' placeholder='SS' />
+          <input id='hours' placeholder='HH' onChange={handleInput}/>
+          <input id='minutes' placeholder='MM' onChange={handleInput}/>
+          <input id='seconds' placeholder='SS' onChange={handleInput}/>
         </div>
         <button className='timer-button' onClick={handleStart}>Start</button>
       </div>
@@ -39,7 +73,7 @@ function App() {
 
           <div className="action-box">
             <button className='timer-button'>Pause</button>
-            <button className='timer-button'>Reset</button>
+            <button className='timer-button' onClick={handleReset}>Reset</button>
           </div>
         </div>
       )
