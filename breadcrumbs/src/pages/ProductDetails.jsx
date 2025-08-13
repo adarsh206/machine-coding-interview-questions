7fb1684d44353b6662a4db1eb881445f7a3a3bcc
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2025-08-13 19:16:13 +0530

    Product Details Page Started

--- breadcrumbs/src/pages/ProductDetails.jsx
+++ breadcrumbs/src/pages/ProductDetails.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const ProductDetails = () => {
+
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+        fetch(`https://dummyjson.com/products/${id}`).then((res) => res.json()).then((res) => {
+          
+          setProduct(res);
+        })
+      }, [])
+
+      console.log(product)
+
   return (
-    <div>ProductDetails</div>
+    <div>
+      <h2>ProductDetails Page</h2>
+    </div>
   )
 }
 
