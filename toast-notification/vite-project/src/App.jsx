86f2663009271db1949d956e5ccb9e171e6ba0be
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-02-10 08:38:38 +0530

    toast notification started

---
+++ toast-notification/vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  // custom hook - useNotification(position)
+
+  return (
+    <div>
+      <h1>Toast Notification</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ toast-notification/vite-project/src/components/notification.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Notification = ({type = "info", message, onClose}) => {
+  return (
+    <div>
+        {/* icon */}
+        {/* message */}
+        {/* close button */}
+    </div>
+  )
+}
+
+export default Notification
