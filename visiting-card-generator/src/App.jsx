9864857b48d7e7db0ea8ed5653b72c4bcdbe268a
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-02-17 08:39:08 +0530

    visiting card generator started

---
+++ visiting-card-generator/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import { Image } from "lucide-react"
+
+function App() {
+ 
+
+  return (
+  <div className='bg-gray-200 h-screen'>
+    <div className='bg-white rounded-xl p-4'>
+     <button className='border border-gray-300 w-24 h-4'>
+      <Image />
+     </button> 
+    </div>
+  </div>
+  )
+}
+
+export default App
