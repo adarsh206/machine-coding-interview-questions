64413f7ad05b1c860ec07b6a20f57b84cab90780
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-02-19 08:29:26 +0530

    book reading tracker started

---
+++ book-reading-tracker/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+   <div className='py-16 h-screen flex items-center justify-center bg-gradient-to-r from-slate-900 via-slate-600 to-slate-900'>
+    <div className='w-9/12 mx-auto p-16 bg-slate-800 h-full border border-slate-600 rounded-xl'>
+      <h1 className='text-white/60 text-5xl font-bold text-center'>Book Library</h1>
+    </div>
+   </div>
+  )
+}
+
+export default App
