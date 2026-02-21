ee8f70e535de52f0415a672c011341c506439f9e
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-02-21 17:48:05 +0530

    Modal added

--- alarm-and-reminder/src/App.jsx
+++ alarm-and-reminder/src/App.jsx
@@ -2,19 +2,19 @@
 import './App.css'
 import 'animate.css'
 import { Plus, Trash2 } from 'lucide-react'
-import { Switch } from 'antd';
+import { DatePicker, Form, Input, Modal, Switch } from 'antd';
 
 function App() {
   
 
   return (
-  <div className='bg-gray-200 min-h-screen py-12'>
-    <div className='bg-white w-7/12 mx-auto shadow-lg rounded-xl animate__animated animate__fadeIn'>
-      <img src='/images/banner.png' className='rounded-t-xl'/>
+  <div className='bg-gray-200 min-h-screen'>
+    <div className='bg-white w-7/12 mx-auto shadow-lg animate__animated animate__fadeIn'>
+      <img src='/images/banner.png' />
 
       <div className='p-8 space-y-6'>
-        <div className='flex justify-between items-center bg-gray-100 p-4 rounded-lg'>
-          <h1 className='text-3xl font-bold'>Coding Alarm</h1>
+        <div className='flex justify-between items-center bg-slate-900 p-4 rounded-lg'>
+          <h1 className='text-3xl font-bold text-white'>Alarms</h1>
           <button className='flex items-center justify-center px-8 py-2 bg-green-500 hover:bg-green-600 transition duration-300 active:scale-80 gap-1 text-white rounded font-medium'>
             <Plus className='w-4 h-4'/>Add Alarm
           </button>
@@ -25,7 +25,7 @@ function App() {
             Array(6).fill(0).map((item, index) => (
               <div key={index} className='bg-white rounded-lg p-4 shadow'>
                 <div className='flex items-center justify-between'>
-                  <h1 className='text-xl font-medium'>06 : 30 AM</h1>
+                  <h1 className='text-2xl font-bold'>06 : 30 AM</h1>
                   <Switch defaultChecked checkedChildren="ON" unCheckedChildren="FF" />
                 </div>
                 <div>
@@ -41,6 +41,18 @@ function App() {
         </div>
       </div>
     </div>
+
+    <Modal open footer={null} title="New Alarm">
+      <Form layout='vertical'>
+        <Form.Item name="title" rules={[{ required : true }]} className='font-medium'>
+          <Input placeholder='Medicine Reminder' size='large'/>
+        </Form.Item>
+
+         <Form.Item name="datetime" rules={[{ required : true }]} className='font-medium'>
+          <DatePicker showTime size='large' className='w-full'/>
+        </Form.Item>
+      </Form>
+    </Modal>
   </div>
   )
 }
