933de44dc8282763db0ffc4336c5377438835192
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-02-25 16:30:33 +0530

    search mark completed

--- search-mark/src/App.jsx
+++ search-mark/src/App.jsx
@@ -8,10 +8,33 @@ function App() {
   const [search, setSearch] = useState('');
 
 
+  const searchText = (data, keyword) => {
+    if(!keyword){
+      return data;
+    }
+    
+    const regEx = new RegExp(`(${keyword})`, 'gi');
+    const parts = data.split(regEx)
+
+    return parts.map((part, index) => {
+      if(part.toLowerCase() === keyword.toLowerCase()){
+        
+        return (
+        <mark key={index} className='bg-yellow-300 text-black px-1'>
+          {part}
+        </mark>
+        )
+      }
+      else{
+        return part
+      }
+    })
+  }
+
   return (
     <div className='bg-gray-200 min-h-screen'>
-      <div className='sticky top-0 left'>
-        <input className='p-6 w-full bg-white focus:outline-none text-lg'
+      <div className='sticky w-8/12 top-0 mx-auto'>
+        <input className='p-6 w-260 mx-12 bg-white focus:outline-none text-lg'
         placeholder='Search content here'
         value={search} onChange={(e) => setSearch(e.target.value.trim())}/>
       </div>
@@ -19,9 +42,11 @@ function App() {
       <div className='w-8/12 mx-auto p-12 space-y-6'>
       <h1 className='text-3xl font-bold'>Content Finder</h1>
         <textarea rows={3} className='bg-white rounded-lg w-full p-6 focus:outline-none'
-        value={content} placeholder='Paste your content here...' onChange={(e) => setContent(e.target.value.trim())}/>
+        value={content} placeholder='Paste your content here...' onChange={(e) => setContent(e.target.value)}/>
         <div className='w-full bg-white rounded-lg p-6 min-h-[1000px]'>
-          <p className='text-gray-600 text-justify'>{content}</p>
+          <p className='text-gray-600 text-justify'>
+            {searchText(content, search)}
+          </p>
         </div>
       </div>
     </div>
