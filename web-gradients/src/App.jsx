8ff1a98cfdb9a1805a6ac417f940b1728c8a4448
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-02-28 08:17:43 +0530

    web gradient completed

--- web-gradients/src/App.jsx
+++ web-gradients/src/App.jsx
@@ -24,12 +24,27 @@ function App() {
     for(let i = 0; i < num; i++){
       const color1 = getHexColorCode();
       const color2 = getHexColorCode();
+      const color3 = getHexColorCode();
       const degree = Math.floor(Math.random() * 360);
       const degreeString = `${degree}deg`
-      colors.push({
-        gradient : `linear-gradient(${degreeString}, ${color1}, ${color2})`,
-        css : `background : 'linear-gradient(${degreeString}, ${color1}, ${color2})'`,
-      })
+      if(type === 'linear'){
+        colors.push({
+          gradient : `linear-gradient(${degreeString}, ${color1}, ${color2}, ${color3})`,
+          css : `background : 'linear-gradient(${degreeString}, ${color1}, ${color2}, ${color3})'`,
+        })
+      }
+      else if(type === "radial"){
+        colors.push({
+          gradient : `radial-gradient(circle, ${color1}, ${color2})`,
+          css : `background : 'radial-gradient("circle", ${color1}, ${color2})'`,
+        })
+      }
+      else{
+        colors.push({
+          gradient : `conic-gradient(${color1}, ${color2}, ${color3})`,
+          css : `background : 'conic-gradient(${color1}, ${color2}, ${color3})'`,
+        })
+      }
     }
     setGradients(colors)
   }
@@ -41,13 +56,14 @@ function App() {
 
   useEffect(() => {
     generateGradient();
-  }, [num])
+  }, [num, type])
 
   return (
    <div className='min-h-screen bg-white py-12'>
       <div className='w-9/12 mx-auto space-y-8'>
-        <div className='flex justify-between'>
-          <h1 className='text-3xl font-bold'>🎨 Gradient Generator</h1>
+        <div style= {{ background: `linear-gradient(to right, ${getHexColorCode()}, ${getHexColorCode()})` }}
+        className='flex justify-between p-4 rounded-xl'>
+          <h1 className='text-3xl font-bold'>🎨 Gradient Generator - {type}</h1>
           <div className='flex gap-4'>
             <input placeholder='12' onChange={(e) => setNum(Number(e.target.value))} value={num}
             className='border border-slate-300 bg-white rounded-lg w-[100px] p-2'/>
@@ -55,7 +71,10 @@ function App() {
             <select value={type} className='border border-slate-300 bg-white rounded-lg w-[100px] p-2' onChange={(e) => setType(e.target.value)}>
               <option value="linear">Linear</option>
               <option value="radial">Radial</option>
+              <option value="conic">Conic</option>
             </select>
+            <button onClick={generateGradient}
+            className='px-16 py-2 bg-rose-500 text-white rounded font-medium'>Generate</button>
           </div>
         </div>
 
