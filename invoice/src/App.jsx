49188d5c6be24f3bd9fc7b34c8d4e4645fdf2779
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-03-02 14:32:29 +0530

    dynamic form fields added

--- invoice/src/App.jsx
+++ invoice/src/App.jsx
@@ -2,7 +2,8 @@
 
 import './App.css'
 import { Plus, Printer } from 'lucide-react';
-import { Button, DatePicker, Drawer, Form, Input, Select, Tooltip } from 'antd'
+import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
+import { Button, DatePicker, Drawer, Form, Input, Select, Space, Tooltip } from 'antd'
 import { useState } from 'react';
 
 function App() {
@@ -173,6 +174,37 @@ function App() {
         )}
         )        
           }
+
+          <Form.List name="users">
+          {(fields, { add, remove }) => (
+            <>
+              {fields.map(({ key, name, ...restField }) => (
+                <Space key={key} style={{ display: 'flex', marginBottom: 8 }} align="baseline">
+                  <Form.Item
+                    {...restField}
+                    name={[name, 'item']}
+                    rules={[{ required: true, message: 'Item is missing' }]}
+                  >
+                    <Input placeholder="First Name" />
+                  </Form.Item>
+                  <Form.Item
+                    {...restField}
+                    name={[name, 'last']}
+                    rules={[{ required: true, message: 'Missing last name' }]}
+                  >
+                    <Input placeholder="Last Name" />
+                  </Form.Item>
+                  <MinusCircleOutlined onClick={() => remove(name)} />
+                </Space>
+              ))}
+              <Form.Item>
+                <Button type="dashed" onClick={() => add()} block icon={<Plus />}>
+                  Add field
+                </Button>
+              </Form.Item>
+            </>
+          )}
+          </Form.List>
           <Form.Item className='col-span-2'>
             <Button size='large' htmlType='submit' type='primary'>Generate</Button>
           </Form.Item>
