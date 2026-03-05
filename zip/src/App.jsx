ccabf2cd2c7461f830ea1a4ae89c3198ad1eb39d
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-03-05 18:45:31 +0530

    zip completed

--- zip/src/App.jsx
+++ zip/src/App.jsx
@@ -5,17 +5,36 @@ import { saveAs } from 'file-saver';
 
 function App() {
  
-  const createZip = async () => {
-    const zip = new JSZip();
-    zip.file("demo.txt", "hello")
-    const blob = await zip.generateAsync({ type: "blob"});
-    saveAs(blob, "files.zip")
+  // const createZip = async () => {
+  //   const zip = new JSZip();
+  //   zip.file("demo.txt", "hello")
+  //   zip.file("sample.json", JSON.stringify({ name : "hello", salary : 1000 }, null, 2)) // 2 means it will take 2 tap space & null means it will follow proper alignment
+  //   const blob = await zip.generateAsync({ type: "blob"});
+  //   saveAs(blob, "files.zip")
+  // }
+
+  const createZip = async (e) => {
+    try {
+      const zip = new JSZip();
+      const files = e.target.files;
+      Array.from(files).forEach((file) => {
+        zip.file(file.name, file)
+      })
+      const blob = await zip.generateAsync({ type : "blob"})
+      saveAs(blob, "new-compressed.zip");
+
+    } catch (error) {
+      console.log("Error - ", error.message)
+    }
   }
 
-
   return (
-   <div>
-    <button onClick={createZip}>Create Zip</button>
+   <div className='bg-gray-200 h-screen flex items-center justify-center'>
+    <div className='bg-white shadow-lg rounded-2xl p-8 w-lg text-center'>
+      <h1 className='text-4xl font-bold'>Create Zip</h1>
+      <input type='file' multiple className='bg-blue-500 text-white font-medium rounded-lg p-3 mt-4 hover:bg-blue-600 active:scale-80 transition duration-300'
+      onChange={createZip}/>
+    </div>
    </div>
   )
 }
