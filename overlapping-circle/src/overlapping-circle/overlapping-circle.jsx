a3bd12016b85d34c0573a8786b4c2fa2672aa7d8
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-04-23 14:12:28 +0530

    event listener added

--- overlapping-circle/src/overlapping-circle/overlapping-circle.jsx
+++ overlapping-circle/src/overlapping-circle/overlapping-circle.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./style.css";
 
 const OverlappingCircle  = () => {
+
+  useEffect(() => {
+    document.addEventListener("click", handleDocumentClick);
+
+    return () => {
+      document.removeEventListener("click", handleDocumentClick)
+    }
+  }, [])
+
+  function handleDocumentClick(event) {
+    const x = event.clientX;
+    const y = event.clientY;
+  }
+
+
   return (
     <div className='circle'>
         <Circle />
