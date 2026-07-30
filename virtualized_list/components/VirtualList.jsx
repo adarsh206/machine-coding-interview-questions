3fa5567779aac9bd7085efa0d8a3a1f6c1ab76b3
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-07-30 10:30:12 +0530

    start and end indices added

--- virtualized_list/components/VirtualList.jsx
+++ virtualized_list/components/VirtualList.jsx
@@ -5,9 +5,17 @@ const VirtualList = ({ list, height, width, itemHeight }) => {
 
     const [indices, setIndices] = useState([0, Math.floor(height / itemHeight)]);
     const visibleList = list.slice(indices[0], indices[1] + 1);
+
+    const handleScroll = (e) => {
+        const { scrollTop } = e.target;
+        console.log(scrollTop);
+        const newStartIndex = Math.floor(scrollTop / itemHeight);
+        const newEndIndex = newStartIndex + Math.floor(height / itemHeight);
+        setIndices([newStartIndex, newEndIndex]);
+    }
     
   return (
-        <div className="container" style={{ height, width, background: 'grey', overflow: 'auto'}}>
+        <div onScroll={handleScroll} className="container" style={{ height, width, background: 'grey', overflow: 'auto'}}>
             <div style={{ height: list.length * itemHeight}}>
                 {
                     visibleList.map((item) => {
