e8e80688fda0480e3b900b945e46acff75dafc56
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-08-09 09:37:40 +0530

    file uploader started

---
+++ file-uploader/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import FileUploader from './components/FileUploader'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <h1 className="app-title">React File Uploader</h1>
+      <FileUploader />
+
+    </div>
+  )
+}
+
+export default App
---
+++ file-uploader/src/components/FileUploader.jsx
@@ -0,0 +1,10 @@
+
+
+const FileUploader = () => {
+  return (
+    <div className="file-uploader">
+        File Uploader</div>
+  )
+}
+
+export default FileUploader
