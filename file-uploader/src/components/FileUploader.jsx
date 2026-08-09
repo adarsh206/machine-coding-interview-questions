197035bb1adcf02d188b7ad0b8c71700cdcf2f1f
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-08-09 10:42:59 +0530

    inputs added

--- file-uploader/src/components/FileUploader.jsx
+++ file-uploader/src/components/FileUploader.jsx
@@ -1,9 +1,20 @@
 
 
 const FileUploader = () => {
+
+    const handleChange = (e) => {
+        console.log(e);
+    }
+    
   return (
     <div className="file-uploader">
-        File Uploader</div>
+        {/** Drag And Drop Zone */}
+        <div className="dropzone">
+            <p>Drag and Drop File here or</p>
+            <input onChange={handleChange} type="file" multiple className="hidden-input" id="file-input"/>
+            <label htmlFor="file-input">Browser Files</label>
+        </div>
+    </div>
   )
 }
 
