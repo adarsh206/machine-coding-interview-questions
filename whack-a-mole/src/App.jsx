f85a7f47e244ee7735500e901b416ec4088eec31
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-08-14 12:12:12 +0530

    whack a mole started

---
+++ whack-a-mole/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import WhackAMole from './components/WhackAMole'
+
+function App() {
+
+  return (
+    <div className="App">
+      <WhackAMole size={3} />
+    </div>
+  )
+}
+
+export default App
---
+++ whack-a-mole/src/components/WhackAMole.jsx
@@ -0,0 +1,9 @@
+
+
+const WhackAMole = ({ size }) => {
+  return (
+    <div>WhackAMole</div>
+  )
+}
+
+export default WhackAMole
