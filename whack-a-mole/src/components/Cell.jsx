3027c6fbcfe8e84c51538ec6ae2d152229277ae9
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-08-15 10:40:05 +0530

    grid added

---
+++ whack-a-mole/src/components/Cell.jsx
@@ -0,0 +1,11 @@
+
+
+const Cell = ({ row, col }) => {
+  return (
+    <div className="cell">
+        Cell
+    </div>
+  )
+}
+
+export default Cell
--- whack-a-mole/src/components/WhackAMole.jsx
+++ whack-a-mole/src/components/WhackAMole.jsx
@@ -1,8 +1,18 @@
+import Cell from "./Cell"
 
 
 const WhackAMole = ({ size }) => {
   return (
-    <div>WhackAMole</div>
+    <div>
+        {Array.from({ length: size }).map((value, row) => {
+            return (
+                <div className="row" key={row}>
+                    {Array.from({ length: size }).map((_, col) => {
+                        return <Cell key={`${row}, ${col}`} />
+                    })}
+                </div>
+        )})}
+    </div>
   )
 }
 
