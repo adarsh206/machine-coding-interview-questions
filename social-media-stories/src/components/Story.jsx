99a45c7562ca441fe7e25594f20879ae2b5daec3
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-08-18 14:52:33 +0530

    input added

--- social-media-stories/src/components/Story.jsx
+++ social-media-stories/src/components/Story.jsx
@@ -3,11 +3,19 @@ const closeTime = 3000;
 const STORY_LIFETIME = 60_000;
 
 const Story = () => {
+
+    const handleChange = (e) => {
+  
+        const file = e.target.files[0];
+        console.log(file)
+        const imageUrl = URL.createObjectURL(file);
+    }
+
   return (
     <>
         <div className="story-row">
             <label className="add-story">+
-                <input type="file" hidden />
+                <input type="file" hidden onChange={(e) => handleChange(e)}/>
             </label>
 
             <div aria-label="preview-story" className="story-preview">
