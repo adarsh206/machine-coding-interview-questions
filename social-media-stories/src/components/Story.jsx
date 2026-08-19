be83a6f7b1854f695ca30d68dab671cb418956a7
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-08-19 07:52:54 +0530

    images array added

--- social-media-stories/src/components/Story.jsx
+++ social-media-stories/src/components/Story.jsx
@@ -1,14 +1,17 @@
+import { useState } from "react";
 
 const closeTime = 3000;
 const STORY_LIFETIME = 60_000;
 
 const Story = () => {
 
+    const [previewStoriesUrl, setPreviewStoriesUrl] = useState([]);
     const handleChange = (e) => {
   
         const file = e.target.files[0];
         console.log(file)
         const imageUrl = URL.createObjectURL(file);
+        setPreviewStoriesUrl((prev) => [imageUrl, ...prev]);
     }
 
   return (
@@ -17,10 +20,17 @@ const Story = () => {
             <label className="add-story">+
                 <input type="file" hidden onChange={(e) => handleChange(e)}/>
             </label>
+            {
+                previewStoriesUrl.map((story) => {
+                    return (
+                        <div aria-label="preview-story" className="story-preview">
+                            <img src={story} alt="Story preview"/>
+                        </div>
+                    )
+                })
+            }
 
-            <div aria-label="preview-story" className="story-preview">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsubI1xnS2EsbFC7IKOtHXy3o2yp5zNGHX8-mLk-0nVw&s" alt="story preview"/>
-            </div>
+            
         </div>
     </>
 
