cd62c7c204ef85c3614595dfde1940ce157011aa
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2026-08-22 13:53:10 +0530

    modal added

--- social-media-stories/src/components/Story.jsx
+++ social-media-stories/src/components/Story.jsx
@@ -6,6 +6,8 @@ const STORY_LIFETIME = 15_000;
 const Story = () => {
 
     const [previewStoriesUrl, setPreviewStoriesUrl] = useState([]);
+    const [storyUrl, setStoryUrl] = useState();
+
     const handleChange = (e) => {
   
         const file = e.target.files[0];
@@ -28,6 +30,10 @@ const Story = () => {
         setPreviewStoriesUrl((prev) => [storyObj, ...prev]);
     }
 
+    const handleStoryClick = (url) => {
+        setStoryUrl(url)
+    }
+
   return (
     <>
         <div className="story-row">
@@ -35,10 +41,10 @@ const Story = () => {
                 <input type="file" hidden onChange={(e) => handleChange(e)}/>
             </label>
             {
-                previewStoriesUrl.map((story) => {
+                previewStoriesUrl.map(({id, imageUrl}) => {
                     return (
-                        <div key={story.id} aria-label="preview-story" className="story-preview">
-                            <img src={story.imageUrl} alt="Story preview"/>
+                        <div key={id} aria-label="preview-story" className="story-preview" onClick={() => handleStoryClick(imageUrl)}>
+                            <img src={imageUrl} alt="Story preview"/>
                         </div>
                     )
                 })
